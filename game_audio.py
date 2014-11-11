f31ee9cf5d9f981afdee98d80e2462549e15804a
Jay S. <jayvsmith@gmail.com>
2014-11-10 23:04:08 -0600

    Copyright notice

--- game_audio.py
+++ game_audio.py
@@ -1,3 +1,12 @@
+# Copyright Skullspace, 2014
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+# http://www.gnu.org/prep/maintain/html_node/License-Notices-for-Other-Files.html
+# @author Jay Smith <jayvsmith@gmail.com>
+
 import ConfigParser
 from os import system, getenv
 from os.path import exists, join
